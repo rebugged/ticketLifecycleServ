cad40f42a4bd985fe24a47eae640efe971c1b893
rebugged <dasabhishek0790@gmail.com>
2023-05-05 02:38:07 +0530

    initial commit ..

---
+++ src/main/java/com/ticketsystem/ticketLifecycleServ/TicketLifecycleServApplication.java
@@ -0,0 +1,13 @@
+package com.ticketsystem.ticketLifecycleServ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketLifecycleServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketLifecycleServApplication.class, args);
+	}
+
+}
